cd7c817582b49c068c69d92322c82004c75c4b81
Andreas Stokholm <asn@asn24.dk>
2014-05-13 20:48:22 +0200

    Added manifest for sidebar icon resize class.

---
+++ manifests/global/sidebar_icon_size_spec.pp
@@ -0,0 +1,22 @@
+# Public: Set the size of sidebar icons.
+# size - Size for icons in sidebar. Defaults to 1, other possible values are 2 and 3.
+#
+# Examples
+#
+#   # set the icon size to 1 (the default)...
+#   include osx::global::sidebar_icon_size
+#
+#   # ...or pick your own size!
+#   class { 'osx::global::sidebar_icon_size':
+#     size => 2
+#   }
+
+class osx::global::sidebar_icon_size($size = 1) {
+  boxen::osx_defaults { 'sidebar icon size':
+	ensure => present,
+	domain => 'NSGlobalDomain',
+	key    => 'NSTableViewDefaultSizeMode',
+	value  => $size,
+	user   => $::boxen_user;
+  }
+}
